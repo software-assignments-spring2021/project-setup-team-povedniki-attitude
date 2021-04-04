c5a984a2a271d3801bdab5d5d7c9f5bcdfb1d533
Dhruv Shetty <dhruvshetty@Dhruvs-Air.cable.rcn.com>
2021-04-04 17:27:18 -0400

    shows full list of drinks

--- front-end/src/Components/CompleteDrinkList/complete_drink_list.js
+++ front-end/src/Components/CompleteDrinkList/complete_drink_list.js
@@ -4,23 +4,93 @@ import Cocktail_Item from './Cocktail_Item'
 import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
 import axios from 'axios';
 
-function CompleteDrinkList(){
 
 
-    return(
-        <>
-        <HamburgerMenu></HamburgerMenu>
-        <header className="App-header">
+function Cocktail_Search_Page(){
+  const [cocktailItems, setCocktails] = useState([]);
+
+  function filterIngredients(rawData){
+    rawData.map((data)=>{
+      let ingredients =[]
+      for (const[key,value] of Object.entries(data)){
+        if(key.includes("strIngredient") &&value!== ""&& value!==null){
+          ingredients.push(value)
+        }
+      }
+      data["ingredients"]= ingredients;
+    })
+  }
+
+  useEffect(() => {
+    axios('https://www.thecocktaildb.com/api/json/v2/9973533/search.php?s=')
+      .then( (response) => {
+        filterIngredients(response.data.drinks)
+        setCocktails(response.data.drinks);
+      })
+      .catch( (err) => {
+        console.log(err);
+      })
+      
+  }, [])
+  const onSubmit = e => {
+    e.preventDefault();
+    
+    let inputDrink = e.currentTarget.previousElementSibling.value;
+    axios.get(`http://localhost:3000/searchpage?search=${inputDrink}`)
+    .then(function (response) {
+      filterIngredients(response.data)
+      setCocktails(response.data);
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  };
+
+  return(
+    <>
+      <HamburgerMenu></HamburgerMenu>
+      <header className="App-header">
         <img id='logo' src='logo.png'></img>
-        </header>
+      </header>
+      <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}>
+            <p><u>Drink Directory</u></p>
+        </div>
+      <main>
+        <div id="SearchContainer">
+          {
+            cocktailItems !== null?
+            cocktailItems.map((item, index)=> {
+             return(
+               <>
+               <Cocktail_Item key ={index} 
+               name={item.strDrink}
+               image= {item.strDrinkThumb}
+               ingredients={item.ingredients}/>
+               {console.log(item)}
+               </>
+             ) 
+            })
+            : <div id = "noResults">No results found!</div>
 
-        <footer>
-          <p>Copyright © 2021 BottomsUp</p>
-        </footer>
+          }
+        </div>
+        
+      </main>
+      <footer>
+        <p>Copyright © 2021 BottomsUp</p>
+      </footer>
+    </>
+  )
 
-      </>
-    )
-
+    
 }
 
-export default CompleteDrinkList;
+export default Cocktail_Search_Page;
+  
+  
