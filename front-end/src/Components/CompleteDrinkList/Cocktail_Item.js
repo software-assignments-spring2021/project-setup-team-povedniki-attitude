12c2430f2881bb9eaabe34f8abf483137d59eff5
Dhruv Shetty <dhruvshetty@Dhruvs-Air.cable.rcn.com>
2021-04-04 14:37:38 -0400

    Implementing complete drink list from task #72

---
+++ front-end/src/Components/CompleteDrinkList/Cocktail_Item.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+
+
+function Cocktail_Item({name,image,ingredients}){
+    return(
+        <Link to={{
+            pathname: "/drinkrecipe",
+            state: {
+                name,
+                image,
+                ingredients,
+                }
+            }}>
+        <div className="Cocktail_Item">
+            <h3 className="name">{name}</h3>
+           {/* </div><div className="Name"> {name}</div> */}
+
+           <div className="content">
+           <img src ={image} alt={''}/>
+           
+           {/* <div className="ingredients">{ingredients}</div> */}
+           <div className= "ingredientsContainer">
+           <h4>Ingredients needed:</h4>
+           <ul>
+             {
+                ingredients.map((ingredient) => {
+                return (
+                      <li className= "ingredients">{ingredient}</li>
+                    )
+                })
+             } 
+             </ul>
+           
+            </div>
+            </div>  
+        </div>  
+        </Link>
+    )
+}
+export default Cocktail_Item;
+  
--- front-end/src/Components/CompleteDrinkList/complete_drink_list.js
+++ front-end/src/Components/CompleteDrinkList/complete_drink_list.js
@@ -1,12 +1,25 @@
 import './complete_drink_list.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Cocktail_Item from './Cocktail_Item'
 import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
+import axios from 'axios';
 
 function CompleteDrinkList(){
 
 
     return(
-        <p>Hello World</p>
+        <>
+        <HamburgerMenu></HamburgerMenu>
+        <header className="App-header">
+        <img id='logo' src='logo.png'></img>
+        </header>
+
+
+        <footer>
+          <p>Copyright © 2021 BottomsUp</p>
+        </footer>
+
+      </>
     )
 
 }
