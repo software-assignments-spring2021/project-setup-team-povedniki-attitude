bb09cf06943d7565a1d1c28271eb55a7c8804adb
Charles Condon <charlescon@live.com>
2021-04-04 21:35:15 -0400

    connected ingredients to results page

--- front-end/src/Components/CocktailList/cocktail_list.js
+++ front-end/src/Components/CocktailList/cocktail_list.js
@@ -1,30 +1,50 @@
 import HamburgerMenu from '../HamburgerMenu/HamburgerMenu';
 import './cocktail_list.css';
-import React from 'react';
-import Cocktail_Item from './Cocktail_Item'
+import React, {useState, useEffect} from 'react';
+import Cocktail_Item from './Cocktail_Item';
+import axios from 'axios';
 
 
 function Cocktail_List(){
+  const [cocktailItems, setCocktails] = useState([]);
 
-    let cocktailItems=[{
-        name: "Martini",
-        matchScore: 99,
-        image:"https://www.liquor.com/thmb/hY8vXSQUQNgWxLN1c-3BntZo5fM=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__liquor__2017__03__07152853__moscow-mule-720x720-recipe-61ce552013b04880901b2abfb63017fd.jpg",
-        description: "vulputate nonummy maecenas tincidunt lacus at velit vivamus vel nulla eget eros elementum pellentesque quisque porta volutpat erat quisque erat eros viverra eget congue eget semper rutrum nulla nunc purus phasellus in felis donec",
-        ingredients: ["Tequila", "Lime"]
-    },{
-        name: "Gin and Tonic",
-        matchScore: 75,
-        image:"https://www.liquor.com/thmb/hY8vXSQUQNgWxLN1c-3BntZo5fM=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__liquor__2017__03__07152853__moscow-mule-720x720-recipe-61ce552013b04880901b2abfb63017fd.jpg",
-        description: "suspendisse ornare consequat lectus in est risus auctor sed tristique in tempus sit amet sem fusce consequat nulla nisl nunc nisl duis bibendum felis sed",
-        ingredients: "Tequila, Lime"
-    },{
-        name: "Vodka Soda",
-        matchScore: 60,
-        image:"https://www.liquor.com/thmb/hY8vXSQUQNgWxLN1c-3BntZo5fM=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__liquor__2017__03__07152853__moscow-mule-720x720-recipe-61ce552013b04880901b2abfb63017fd.jpg",
-        description: "mauris eget massa tempor convallis nulla neque libero convallis eget eleifend luctus ultricies eu nibh quisque id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae nulla dapibus",
-        ingredients: "Tequila, Lime"
-    },]
+  useEffect(() => {
+
+    let storage = (window.localStorage.getItem('ingredients'));
+    storage = storage.split("\"");
+    for (let j = 0; j < storage.length; j++) {
+      storage.splice(j, 1);
+    }
+
+    axios(`https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=${storage.join(",")}`)
+      .then( (response) => {
+        setCocktails(response.data.drinks);
+      })
+      .catch( (err) => {
+        console.log(err);
+      })
+      
+  }, [])
+
+    // let cocktailItems=[{
+    //     name: "Martini",
+    //     matchScore: 99,
+    //     image:"https://www.liquor.com/thmb/hY8vXSQUQNgWxLN1c-3BntZo5fM=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__liquor__2017__03__07152853__moscow-mule-720x720-recipe-61ce552013b04880901b2abfb63017fd.jpg",
+    //     description: "vulputate nonummy maecenas tincidunt lacus at velit vivamus vel nulla eget eros elementum pellentesque quisque porta volutpat erat quisque erat eros viverra eget congue eget semper rutrum nulla nunc purus phasellus in felis donec",
+    //     ingredients: ["Tequila", "Lime"]
+    // },{
+    //     name: "Gin and Tonic",
+    //     matchScore: 75,
+    //     image:"https://www.liquor.com/thmb/hY8vXSQUQNgWxLN1c-3BntZo5fM=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__liquor__2017__03__07152853__moscow-mule-720x720-recipe-61ce552013b04880901b2abfb63017fd.jpg",
+    //     description: "suspendisse ornare consequat lectus in est risus auctor sed tristique in tempus sit amet sem fusce consequat nulla nisl nunc nisl duis bibendum felis sed",
+    //     ingredients: "Tequila, Lime"
+    // },{
+    //     name: "Vodka Soda",
+    //     matchScore: 60,
+    //     image:"https://www.liquor.com/thmb/hY8vXSQUQNgWxLN1c-3BntZo5fM=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__liquor__2017__03__07152853__moscow-mule-720x720-recipe-61ce552013b04880901b2abfb63017fd.jpg",
+    //     description: "mauris eget massa tempor convallis nulla neque libero convallis eget eleifend luctus ultricies eu nibh quisque id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae nulla dapibus",
+    //     ingredients: "Tequila, Lime"
+    // },]
 
     return(
         <>
@@ -47,11 +67,9 @@ function Cocktail_List(){
                     <>
                     <Cocktail_Item key ={index} 
                     //this is hardcoded for now
-                    name={item.name}
-                    matchScore = {item.matchScore}
-                    image= {item.image}
-                    description={item.description}
-                    ingredients = {item.ingredients}/>
+                    name={item.strDrink}
+                    image= {item.strDrinkThumb}
+                    drinkID = {item.idDrink}/>
                     </>
                 ) 
             })
